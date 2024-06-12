2a686850fb06c0ae848d987c06ee5eb8c877bf04
eifal <syahrifal7@gmail.com>
2024-06-13 06:21:07 +0700

    fix position

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,15 @@ import Head from 'next/head';
 export default function Home() {
   return (
     <AuroraBackground className='bg-black'>
+    <div className='body'>
       <div className="centered-box">
         <EvervaultCard className='text-shadow' text='Eifal-運命' imageUrl='/unmei.png'>
         </EvervaultCard>
       </div>
-      <div className='button-box'>
-        <SocialButtons/>
+      <div className='spacing'>
+        <SocialButtons />
       </div>
+    </div>
     </AuroraBackground>
   );
 }
