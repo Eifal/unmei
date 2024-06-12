915cbf54ac2c2091263b41a9f06f7f723de4b998
eifal <syahrifal7@gmail.com>
2024-06-13 05:49:35 +0700

    nothing

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 export default {
-  distDir: 'build', // Direktori output build
+  distDir: 'build',
 
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'images.freeimages.com',
-        pathname: '/365/images/previews/**', // Sesuaikan pathname sesuai kebutuhan
+        pathname: '/365/images/previews/**',
       },
     ],
   },
